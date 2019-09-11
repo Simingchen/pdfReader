dd56badb961df42a07c05e91c274dc49b24218c1
Simingchen <siming535@gmail.com>
2019-09-11 08:34:05 +0800

    修改标签

--- js/bookmark_config.js
+++ js/bookmark_config.js
@@ -1,1 +1,63 @@
-var ols = [{"caption":"\u9875 1","page":"1","url":"","level":"1","children":[]},{"caption":"\u9875 2","page":"3","url":"","level":"1","children":[]},{"caption":"\u9875 3","page":"5","url":"","level":"1","children":[]},{"caption":"\u9875 4","page":"7","url":"","level":"1","children":[]},{"caption":"\u9875 5","page":"9","url":"","level":"1","children":[]},{"caption":"6: \u88c5\u8ba2\u9875","page":"11","url":"","level":"1","children":[]},{"caption":"\u9875 7","page":"13","url":"","level":"1","children":[]},{"caption":"\u9875 8","page":"15","url":"","level":"1","children":[]},{"caption":"\u9875 9","page":"17","url":"","level":"1","children":[]},{"caption":"\u9875 10","page":"19","url":"","level":"1","children":[]}];
+// 书本目录，如果无ols变量则无目录
+// 后台做成用户自行添加目录
+var ols = [{
+  "caption": "测试页面",
+  "page": "1",
+  "url": "",
+  "level": "1",
+  "children": []
+}, {
+  "caption": "\u9875 2",
+  "page": "3",
+  "url": "",
+  "level": "1",
+  "children": []
+}, {
+  "caption": "\u9875 3",
+  "page": "5",
+  "url": "",
+  "level": "1",
+  "children": []
+}, {
+  "caption": "\u9875 4",
+  "page": "7",
+  "url": "",
+  "level": "1",
+  "children": []
+}, {
+  "caption": "\u9875 5",
+  "page": "9",
+  "url": "",
+  "level": "1",
+  "children": []
+}, {
+  "caption": "6: 彭于晏的",
+  "page": "11",
+  "url": "",
+  "level": "1",
+  "children": []
+}, {
+  "caption": "\u9875 7",
+  "page": "13",
+  "url": "",
+  "level": "1",
+  "children": []
+}, {
+  "caption": "\u9875 8",
+  "page": "15",
+  "url": "",
+  "level": "1",
+  "children": []
+}, {
+  "caption": "\u9875 9",
+  "page": "17",
+  "url": "",
+  "level": "1",
+  "children": []
+}, {
+  "caption": "\u9875 10",
+  "page": "19",
+  "url": "",
+  "level": "1",
+  "children": []
+}];
